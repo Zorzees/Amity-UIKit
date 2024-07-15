7466effca5b6f20302dc18a7d3185ee943f112dc
Guillaume Kanoufi <g.kanoufi@gmail.com>
2024-07-15 15:44:08 +0200

    Amity Fork and updates

--- src/social/components/SideSectionCommunity/index.tsx
+++ src/social/components/SideSectionCommunity/index.tsx
@@ -13,37 +13,37 @@ export const NewsIcon = styled(Newspaper).attrs<{ icon?: ReactNode }>({ width: 2
 export const SearchIcon = styled(Search).attrs<{ icon?: ReactNode }>({ width: 20, height: 20 })``;
 
 interface SideSectionCommunityProps {
-  shouldHideExplore?: boolean;
-  children?: ReactNode;
+	shouldHideExplore?: boolean;
+	children?: ReactNode;
 }
 
 const SideSectionCommunity = ({ shouldHideExplore, children }: SideSectionCommunityProps) => {
-  const { onChangePage, page } = useNavigation();
+	const { onChangePage, page } = useNavigation();
 
-  return (
-    <SideMenuSection heading={<FormattedMessage id="sidesectioncommunity.community" />}>
-      <SideMenuActionItem
-        data-qa-anchor="side-section-community-side-menu-action-item-news-feed-button"
-        icon={<NewsIcon />}
-        active={page.type === PageTypes.NewsFeed}
-        onClick={() => onChangePage(PageTypes.NewsFeed)}
-      >
-        <FormattedMessage id="sidesectioncommunity.newfeed" />
-      </SideMenuActionItem>
+	return (
+		<SideMenuSection heading={<FormattedMessage id="sidesectioncommunity.community" />}>
+			<SideMenuActionItem
+				data-qa-anchor="side-section-community-side-menu-action-item-news-feed-button"
+				icon={<NewsIcon />}
+				active={page.type === PageTypes.NewsFeed}
+				onClick={() => onChangePage(PageTypes.NewsFeed)}
+			>
+				<FormattedMessage id="sidesectioncommunity.newfeed" />
+			</SideMenuActionItem>
 
-      {!shouldHideExplore && (
-        <SideMenuActionItem
-          data-qa-anchor="side-section-community-side-menu-action-item-explore-button"
-          icon={<SearchIcon />}
-          active={page.type === PageTypes.Explore}
-          onClick={() => onChangePage(PageTypes.Explore)}
-        >
-          <FormattedMessage id="sidesectioncommunity.explore" />
-        </SideMenuActionItem>
-      )}
-      {children}
-    </SideMenuSection>
-  );
+			{!shouldHideExplore && (
+				<SideMenuActionItem
+					data-qa-anchor="side-section-community-side-menu-action-item-explore-button"
+					icon={<SearchIcon />}
+					active={page.type === PageTypes.Explore}
+					onClick={() => onChangePage(PageTypes.Explore)}
+				>
+					<FormattedMessage id="sidesectioncommunity.explore" />
+				</SideMenuActionItem>
+			)}
+			{children}
+		</SideMenuSection>
+	);
 };
 
 export default SideSectionCommunity;
