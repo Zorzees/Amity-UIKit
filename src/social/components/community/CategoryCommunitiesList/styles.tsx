36d102812d6e7de747982978aaab6ea30191a8ab
Guillaume Kanoufi <g.kanoufi@gmail.com>
2025-01-15 17:20:17 +0100

    Updated grid max width

--- src/social/components/community/CategoryCommunitiesList/styles.tsx
+++ src/social/components/community/CategoryCommunitiesList/styles.tsx
@@ -3,7 +3,8 @@ import EmptyState from '~/core/components/EmptyState';
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, calc(50% - 1rem));
+  // grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 1rem;
 `;
 
