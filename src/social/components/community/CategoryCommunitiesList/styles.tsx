fdc0e893df040738885728dc98f74b5b0e97bb0c
Guillaume Kanoufi <g.kanoufi@gmail.com>
2025-08-11 18:01:19 +0200

    Some community list updates

--- src/social/components/community/CategoryCommunitiesList/styles.tsx
+++ src/social/components/community/CategoryCommunitiesList/styles.tsx
@@ -3,8 +3,9 @@ import EmptyState from '~/core/components/EmptyState';
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, calc(50% - 1rem));
   gap: 1rem;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(250px, calc(50% - 1rem)));
 `;
 
 export const ListEmptyState = styled(EmptyState)`
